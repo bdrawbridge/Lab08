290223d2f63f363205a478b5106b727a10bc2056
bdrawbridge <bdrawbridge@my.uri.edu>
2022-03-28 15:25:06 -0400

    Added Facebook.py

---
+++ Facebook.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your Facebook username: ")
+userPassword = input("Hello! Please enter your Facebook password: ")
+
+if(userName.lower() == "bowen" and userPassword == "bowen123"):
+    print("Welcome to Facebook!")
+else:
+    print("Wrong credenials, try again!")
